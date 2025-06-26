a695de3c8e779888e5f0888123a056de3aa5686e
Chrisreynolds0508 <Chris.reynolds0508@gmail.com>
2025-06-26 14:53:59 +1000

    added all my timer logic

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ document.body.appendChild(container);
 
 const display = document.createElement('div');
 display.classList.add('display');
+display.textContent = 'Time: 00:00:00:000';
 container.appendChild(display);
 
 const startButton = document.createElement('button');
@@ -16,5 +17,60 @@ stopButton.classList.add('stopButton');
 stopButton.textContent = 'Stop';
 container.appendChild(stopButton);
 
-let initialState = Date.now();
-let timerStopped = true; 
+const resetButton = document.createElement('button');
+resetButton.textContent = 'Reset';
+resetButton.classList.add('resetButton');
+container.appendChild(resetButton)
+
+let startTime;
+let interval; 
+let totalElapsed = 0;
+let timerStarted  = false; 
+
+const formatTime = (ms) => {
+    const hours = Math.floor(ms/3600000);
+    const minutes = Math.floor((ms % 3600000) / 60000);
+    const seconds = Math.floor((ms % 60000) / 1000);
+    const milliseconds = Math.floor(ms % 1000);
+
+    return (
+        String(hours).padStart(2,'0') + ':' +
+        String(minutes).padStart(2,'0') + ':' +
+        String(seconds).padStart(2,'0') + ':' +
+        String(milliseconds).padStart(3,'0')
+    );
+}
+
+const start = () => {
+   startButton.addEventListener('click', () => {
+    if(!timerStarted){
+        timerStarted = true;
+        startTime = Date.now();
+
+        interval = setInterval(() => {
+            const currentElapsed = Date.now() - startTime;
+            const totalTime = totalElapsed + currentElapsed;
+            display.textContent = `Time: ${formatTime(totalTime)}`
+        }, 10)
+    }
+    
+   })
+}
+
+
+stopButton.addEventListener('click', () => {
+    if(timerStarted){
+    clearInterval(interval);
+    timerStarted = false;
+    totalElapsed += Date.now() - startTime;
+    }
+}) 
+
+resetButton.addEventListener('click', () => {
+    clearInterval(interval);
+    timerStarted = false;
+     totalElapsed = 0;
+    display.textContent = 'Time: 00:00:00:000'
+});
+start();
+
